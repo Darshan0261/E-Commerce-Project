d594019aaef3d4ce8ca562bda2226f39b14fe847
Darshan <darshannas11@gmail.com>
2023-06-02 22:29:49 +0530

    Product filtering, sorting, selection of fields and pagination added

--- backend/controllers/productCtrl.js
+++ backend/controllers/productCtrl.js
@@ -28,7 +28,45 @@ const getaProduct = asyncHandler(async (req, res) => {
 });
 
 const getAllProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find();
+    // Filtering
+    let queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Product.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(",").join(" ");
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort("-createdAt");
+    }
+
+    // Selecting Fields
+    if (req.query.fields) {
+        const fields = req.query.fields.split(",").join(" ");
+        query = query.select(fields);
+    } else {
+        query = query.select("-__v");
+    }
+
+    // Pagination
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 2;
+    const skip = (page - 1) * limit;
+
+    if (req.query.page) {
+        const productsCount = await Product.countDocuments();
+        if (skip >= productCount) {
+            throw new Error("This page does not exists");
+        }
+    }
+
+    query = query.skip(skip).limit(limit);
+
+    const products = await query;
     res.json(products);
 });
 
